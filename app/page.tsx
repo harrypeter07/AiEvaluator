9f6e73208a0f159c5b68a7b6ae07559544848e6a
harrypeter07 <hassanmansuri570@gmail.com>
2025-03-09 21:35:59 +0530

    just text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="h-full w-full">
-        
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, ipsam corrupti quae illo quisquam, assumenda maxime, iure culpa cupiditate est mollitia fugit alias totam rerum vero non iusto iste natus atque ducimus recusandae? Odit, recusandae. A ipsam ratione officiis ad provident earum, rem at iure illum eligendi alias nam cumque illo perferendis id, debitis sunt error ducimus deserunt vitae possimus asperiores neque distinctio! Sit cumque dignissimos dicta blanditiis quod enim dolorem est debitis asperiores repellendus officia, quidem voluptate, dolore quia alias! Qui nostrum aut iusto, dolore deleniti placeat, culpa architecto ad iste veritatis ab praesentium itaque, voluptates enim repellat. Voluptatibus.
     </div>
   );
 }
